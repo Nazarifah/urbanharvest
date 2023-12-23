f1b0c326cf4a6fa2038a7b75eaabe33584a37916
9979Gyu <yuqin1161@gmail.com>
2023-12-23 10:08:14 +0800

    correcting path

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function getDate(isExpired){
+function getDate(isExpired, bookYear){
     var currentDT = new Date();
     var day = currentDT.getDate().toString().padStart(2, '0');
     var month = (currentDT.getMonth() + 1).toString().padStart(2, '0'); 
@@ -8,8 +8,15 @@ function getDate(isExpired){
     var second = currentDT.getSeconds().toString().padStart(2, '0');
 
     if(isExpired){
-        year += 1;
+        bookYear = parseInt(bookYear);
+        year += bookYear;
     }
 
     return day + '/' + month + '/' + year + ' ' + hour + ':' + minute + ':' + second;
+}
+
+function scrollToMessage() {
+    $('html, body').animate({
+        scrollTop: $(".message").offset().top
+    }, 'slow');
 }
