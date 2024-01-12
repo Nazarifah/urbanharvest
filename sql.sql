022d2c72b974bee539403993dbebbe2a546b12ad
9979Gyu <yuqin1161@gmail.com>
2024-01-12 20:02:36 +0800

    updated sql

--- sql.sql
+++ sql.sql
@@ -1,66 +1,66 @@
 CREATE TABLE IF NOT EXISTS ROLE (
     roleID INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
+    name VARCHAR(255) NOT NULL,
     description VARCHAR(255),
-    status INT
+    status INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS USER (
     userID INT AUTO_INCREMENT PRIMARY KEY,
-    firstName VARCHAR(255),
-    lastName VARCHAR(255),
-    email VARCHAR(255),
-    contactNo VARCHAR(255),
-    homeAddress VARCHAR(255),
-    status INT,
-    password VARCHAR(255),
-    roleID INT,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    contactNo VARCHAR(255) UNIQUE NOT NULL,
+    homeAddress VARCHAR(255) NOT NULL,
+    status INT NOT NULL,
+    password TEXT NOT NULL,
+    roleID INT NOT NULL,
     CONSTRAINT FOREIGN KEY (roleID) REFERENCES ROLE(roleID) 
 );
 
 CREATE TABLE IF NOT EXISTS GARDEN (
     gardenID INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    address VARCHAR(255),
-    status INT
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    status INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS PLOT (
     plotID INT AUTO_INCREMENT PRIMARY KEY,
-    size VARCHAR(255),
-    availability INT,
-    status INT,
-    gardenID INT,
+    size VARCHAR(255) NOT NULL,
+    availability INT NOT NULL,
+    status INT NOT NULL,
+    gardenID INT NOT NULL,
     CONSTRAINT FOREIGN KEY (gardenID) REFERENCES GARDEN(gardenID)
 );
 
 CREATE TABLE IF NOT EXISTS BOOKING (
     bookingID INT AUTO_INCREMENT PRIMARY KEY,
-    bookDateTime DateTime,
-    paymentStatus INT,
+    bookDateTime DateTime NOT NULL,
+    paymentStatus INT NOT NULL,
     paymentDateTime DateTime,
     paidAmount DECIMAL(8, 2),
-    bookYear INT,
-    bookApproval INT,
-    status INT,
-    plotID INT,
-    userID INT,
+    bookYear INT NOT NULL,
+    bookApproval INT NOT NULL,
+    status INT NOT NULL,
+    plotID INT NOT NULL,
+    userID INT NOT NULL,
     CONSTRAINT FOREIGN KEY (plotID) REFERENCES PLOT(plotID),
     CONSTRAINT FOREIGN KEY (userID) REFERENCES USER(userID)
 );
 
 CREATE TABLE IF NOT EXISTS QUESTION (
     questionID INT AUTO_INCREMENT PRIMARY KEY,
-    sentence VARCHAR(255),
-    status INT
+    sentence VARCHAR(255) NOT NULL,
+    status INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS ANSWER (
     answerID INT AUTO_INCREMENT PRIMARY KEY,
-    answerSentence VARCHAR(255),
-    status INT,
-    questionID INT,
-    userID INT,
+    answerSentence VARCHAR(255) NOT NULL,
+    status INT NOT NULL,
+    questionID INT NOT NULL,
+    userID INT NOT NULL,
     CONSTRAINT FOREIGN KEY (questionID) REFERENCES QUESTION(questionID),
     CONSTRAINT FOREIGN KEY (userID) REFERENCES USER(userID)
 );
@@ -71,7 +71,8 @@ INSERT INTO ROLE (name, description, status) VALUES
 ("Customer", "People who use the system to book a plot", 1);
 
 INSERT INTO USER (firstName, lastName, email, contactNo, homeAddress, roleID, status, password) VALUES
-("Yu Qin", "Gui", "yuqin1161@gmail.com", "0129231224", "Taman Desa Idaman", 1, 1, "abc123");
+("Yu Qin", "Gui", "yuqin1161@gmail.com", "0129231224", "Taman Desa Idaman", 1, 1, "abc123"),
+("is me", "Customer", "customer@gmail.com", "0129232345", "Taman Desa Idaman", 3, 1, "abc123");
 
 INSERT INTO GARDEN (name, address, status) VALUES
 ('Taman Desa Idaman', 'Taman Desa Idaman, 76100, Durian Tunggal, Melaka', 1),
